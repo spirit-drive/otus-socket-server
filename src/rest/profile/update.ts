978d112e50bb7cd5fa7be3667908327fc36e143a
Igor Zvyagin <spirit-drive@yandex.ru>
2023-09-07 12:10:18 +0300

    feat: made validation handing

--- src/rest/profile/update.ts
+++ src/rest/profile/update.ts
@@ -2,17 +2,25 @@ import { ParamsDictionary, RequestHandler } from 'express-serve-static-core';
 import { Profile } from './types';
 import { UserDocument } from '../../models/User';
 import { prepareProfile } from './prepareProfile';
-import { DataBaseError } from '../../Errors';
+import { DataBaseError, InvalidNickNameError } from '../../Errors';
 
-export const update: RequestHandler<ParamsDictionary, Profile | Error> = async (req, res) => {
+export const update: RequestHandler<ParamsDictionary, Profile | Error> = async (req, res, next) => {
   try {
     const user = req.user as UserDocument;
     const { name } = req.body;
     user.name = name;
-    await user.save();
 
-    res.send(prepareProfile(user));
+    // Выполняем валидацию перед сохранением
+    const validationError = user.validateSync();
+    if (validationError) {
+      // Если есть ошибки валидации, отправляем ValidationError
+      res.status(400).send(new InvalidNickNameError(validationError.message));
+    } else {
+      // Если валидация успешна, сохраняем документ
+      await user.save();
+      res.send(prepareProfile(user));
+    }
   } catch (e) {
-    res.status(400).send(new DataBaseError(e));
+    res.status(500).send(new DataBaseError(e));
   }
 };
