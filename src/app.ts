ab9f83bf7f89f97a1fae4f2dc5da928570d0b79d
Igor Zvyagin <spirit-drive@yandex.ru>
2023-09-11 16:54:44 +0300

    feat: made client

--- src/app.ts
+++ src/app.ts
@@ -6,6 +6,7 @@ import config from './db.config';
 import * as mongoose from 'mongoose';
 import { setRoutes } from './rest/setRoutes';
 import { setSocket } from './socket/setSocket';
+import * as path from 'path';
 
 (async () => {
   const app = express();
@@ -27,6 +28,10 @@ import { setSocket } from './socket/setSocket';
   app.use(cors());
 
   setRoutes(app);
+
+  const root = path.join(process.cwd(), 'dist');
+  app.use(express.static(root));
+  app.get('*', (_, res) => res.sendFile('index.html', { root }));
 
   httpServer.listen(port, () => console.log(`🚀 Server ready at http://localhost:${port}`));
 })();
